734af48678a807820d684bdce12722c0ea5f4237
Mysha2022 <108262051+Mysha2022@users.noreply.github.com>
2024-03-07 20:18:01 +1100

    Add files via upload

---
+++ Project 130/deleted.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# pandas module helps us by reading the data from csv files
+df = pd.read_csv("merged_dataset.csv")
+
+# to check the total number of rows and columns --> shape keyword is used
+print(df.shape)
+
+# (4284, 85) --> (rows , cols)
+
+
+# delete extra columns
+del df["luminosity"]
+del df["NAN"]
+
+
+print(df.shape)
+
+
+df.to_csv("deleted.csv")
